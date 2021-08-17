ed865711375569b811bce1ef03f4b91a478abc96
Chelsea <chelsea.icaza10@hotmail.com>
2021-08-17 02:13:29 -0400

    commit tree 9a15947da7322ab357a2f99b2054ca31b22e17a6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    return(
+        <>
+        <input 
+        type="password" 
+        name="passfield"
+        onChange = {(event) => console.log('Entering password...')}
+        placeholder="Enter PW here..."/>
+        </>
+    );
+
+}
+
+export default Keypad;
